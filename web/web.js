c2bf01435cf026d4917e61708d22ff24f0e1ab3e
Andre Anaya <andre.anaya@gmail.com>
2018-06-20 14:51:23 +0100

    Adjust nginx config for A+ score on SSL Labs and A score on securityheaders.io

--- web/web.js
+++ web/web.js
@@ -9,10 +9,12 @@ app.get('/', (req, res) => {
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/andreanaya.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/andreanaya.com/fullchain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/ssl/certs/dhparam-2048.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
-	cert: certificate
+	cert: certificate,
+	ca: ca
 };
 
 let server = https.createServer(credentials, app).listen(3000, () => {
